0c65327f19916fe41e889bc042df6623b04975bf
Petrov D <PetrovDY@rubezh.ru>
2017-08-10 14:21:04 +0400

    Добавли проект для Medicom Task 1

---
+++ Medicom/Task 1/Task 1/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Task_1.Views;
+using System.Windows;
+
+namespace Task_1
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
